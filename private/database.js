19c3164a247f7eed4b4019815f7ab4847ab80e05
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-09 03:04:14 +0300

    Finish. Replace raw SQL with ORM

--- private/database.js
+++ private/database.js
@@ -81,28 +81,121 @@ function parseData(messages, callback) {
     callback(result);
 }
 
+function listingForBothAnchors(since_id, till_id, callback) {
+    Message
+        .findAll({
+            where: {
+                $or: [
+                    {
+                        id: since_id
+                    },
+                    {
+                        id: till_id
+                    }
+                ]
+            }
+        })
+        .then(boundaries => {
+            if (boundaries.length !== 2) {
+                console.log('Bad request for anchors');
+                return;
+            }
+
+            if (boundaries[0].id === since_id) {
+                return {
+                    till_date: boundaries[1].date,
+                    since_date: boundaries[0].date
+                };
+            } else {
+                return {
+                    till_date: boundaries[0].date,
+                    since_date: boundaries[1].date
+                };
+            }
+        })
+        .then(result => {
+            return Message
+                .findAll({
+                    where: {
+                        date: {
+                            $and: {
+                                $lte: result.till_date,
+                                $gte: result.since_date
+                            }
+                        }
+                    },
+                    order: [
+                        ['date', 'ASC']
+                    ]
+                })
+        })
+        .then(messages => {
+            parseData(messages, callback);
+        })
+        .catch(error => {
+            console.log(error);
+            parseData([], callback);
+        })
+}
+
+function listingForOneAnchor(data) {
+    Message
+        .find({
+            where: {id: data.identifier}
+        })
+        .then(message => {
+            let date = data.isSinceAnchor ? {$gte: message.date} : {$lte: message.date};
+
+            return Message
+                .findAll({
+                    where: {date: date},
+                    order: [['date', 'ASC']],
+                    limit: data.limit,
+                    offset: data.offset
+                })
+        })
+        .then(messages => {
+            parseData(messages, data.callback);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
 exports.listingInRange = function (offset, limit, since_id, till_id, callback) {
 
-    // var query =
-    //     "SELECT messages.id, messages.author, messages.text, messages.date" +
-    //     " FROM Node.messages";
-    //
-    // if (since_id !== undefined && till_id !== undefined) {
-    //     query += " WHERE messages.id < " + since_id + " AND messages.id > " + till_id + " ORDER BY messages.id DESC";
-    // } else {
-    //     if (since_id !== undefined) {
-    //         query += " WHERE messages.id < " + since_id;
-    //     } else {
-    //         query += " WHERE messages.id > " + till_id;
-    //     }
-    //     query += " ORDER BY messages.id DESC" + " LIMIT " + limit + " OFFSET " + offset;
-    // }
-    //
-    // db.query(query, function (err, rows) {
-    //         if (err) throw err;
-    //         parseData(rows, callback);
-    //     }
-    // );
+    if (isNaN(offset) || isNaN(limit)) {
+        parseData([], callback);
+    }
+
+    let data = {};
+    data.callback = callback;
+    data.limit = limit;
+    data.offset = offset;
+
+    const isValidSinceID = since_id !== undefined;
+    const isValidTillID = till_id !== undefined;
+
+    if (isValidSinceID && isValidTillID) {
+
+        listingForBothAnchors(since_id, till_id, callback);
+
+    } else if (isValidSinceID && !isValidTillID) {
+
+        data.isSinceAnchor = true;
+        data.identifier = since_id;
+        listingForOneAnchor(data);
+
+    } else if (!isValidSinceID && isValidTillID) {
+        data.isSinceAnchor = false;
+        data.identifier = till_id;
+
+        listingForOneAnchor(data);
+    }
+    else {
+        // simple paging
+        exports.simpleListing(offset, limit, callback);
+    }
 };
 
 exports.createMessage = function (author, text, callback) {
--- routes/message/listing.js
+++ routes/message/listing.js
@@ -1,31 +1,15 @@
 
 // main function from file always receive 2 params
 exports.route = function (req, res) {
+
     let limit = parseInt(req.body.limit);
     let offset = parseInt(req.body.offset);
-
-    let validNumbers = Number.isInteger(offset) && Number.isInteger(limit);
-    let undefinedValues = limit === undefined || offset === undefined;
-
-    if (undefinedValues || !validNumbers) {
-        res.status(400);
-        res.end();
-        return;
-    }
 
     let sinceId = req.body.since_id;
     let tillId = req.body.till_id;
 
-    if (sinceId === undefined && tillId === undefined) {
-        // both are empty. Simple flow
-        exports.database.simpleListing(offset, limit, function (result) {
-            res.json(result);
-            res.end();
-        });
-    } else {
-        exports.database.listingInRange(offset, limit, sinceId, tillId, function (result) {
-            res.json(result);
-            res.end();
-        })
-    }
+    exports.database.listingInRange(offset, limit, sinceId, tillId, function (result) {
+        res.json(result);
+        res.end();
+    });
 };
